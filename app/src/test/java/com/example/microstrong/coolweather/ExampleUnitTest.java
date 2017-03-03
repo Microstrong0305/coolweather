aa34c9027eb307cb6635768c8a34502488fac254
zhouyanqinag <819565479@qq.com>
2017-03-03 20:55:44 +0800

    first commit

---
+++ app/src/test/java/com/example/microstrong/coolweather/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.example.microstrong.coolweather;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
